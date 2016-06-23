f79bb06044a954b9b4e3c6c11714b137c4b5a7ea
Stewart Crainie <stewartcrainie@gmail.com>
2016-06-23 19:37:00 +0800

    23/06/2017
    Added custom done button colour options.

--- SCDatePickerExample/SCDatePickerExample/ViewController.swift
+++ SCDatePickerExample/SCDatePickerExample/ViewController.swift
@@ -11,26 +11,16 @@ import UIKit
 class ViewController: UIViewController, SCDateFieldDelegate {
 
     @IBOutlet weak var datePickerTextField: DateFieldTextField!
-    @IBOutlet weak var timePickerTextField: DateFieldTextField!
-    @IBOutlet weak var countDownPickerTextField: DateFieldTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        datePickerTextField.dateDelegate = self
-        datePickerTextField.datetype = .date
-        
-<<<<<<< Updated upstream
         //Options
+        datePickerTextField.dateDelegate = self
+        datePickerTextField.datetype = .time
         datePickerTextField.doneButtonTint = UIColor.redColor()
-=======
-        timePickerTextField.dateDelegate = self
-        timePickerTextField.datetype = .time
         
-        countDownPickerTextField.dateDelegate = self
-        countDownPickerTextField.datetype = .countdown
->>>>>>> Stashed changes
     }
 
     
