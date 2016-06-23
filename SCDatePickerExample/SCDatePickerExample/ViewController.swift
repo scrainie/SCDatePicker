74a741e886c4ed32e7013717a627a2467e235918
Stewart Crainie <stewartcrainie@gmail.com>
2016-06-23 19:32:40 +0800

    Commit

--- SCDatePickerExample/SCDatePickerExample/ViewController.swift
+++ SCDatePickerExample/SCDatePickerExample/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController, SCDateFieldDelegate {
 
     @IBOutlet weak var datePickerTextField: DateFieldTextField!
+    @IBOutlet weak var timePickerTextField: DateFieldTextField!
+    @IBOutlet weak var countDownPickerTextField: DateFieldTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +21,16 @@ class ViewController: UIViewController, SCDateFieldDelegate {
         datePickerTextField.dateDelegate = self
         datePickerTextField.datetype = .date
         
+<<<<<<< Updated upstream
         //Options
         datePickerTextField.doneButtonTint = UIColor.redColor()
+=======
+        timePickerTextField.dateDelegate = self
+        timePickerTextField.datetype = .time
+        
+        countDownPickerTextField.dateDelegate = self
+        countDownPickerTextField.datetype = .countdown
+>>>>>>> Stashed changes
     }
 
     
